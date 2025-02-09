e8f56f9fdae3330195e54da4d3804b5467aaa1f1
Catlak Profesor <progesor@gmail.com>
2025-02-09 23:04:44 +0300

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/peronel-schedule/', // GitHub Pages alt dizininiz
+  base: '/peronel-schedule/', // GitHub Pages'deki alt dizin adınız
   plugins: [react()],
 });
